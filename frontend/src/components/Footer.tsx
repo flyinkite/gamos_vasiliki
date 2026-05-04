3b9ab0f385c754cdcddd3b99e9d9fee7f065f32f
Orpheus-Dimitrios Tselos <orfeastselos@gmail.com>
2026-05-04 05:25:04 +0300

    changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,18 @@
 import footerStyle from "./Footer.module.css";
+import { useTranslation } from "react-i18next";
 
 export const Footer = () => {
+  const { t } = useTranslation();
+  
   return (
     <footer className={footerStyle.footer}>
       <div className={footerStyle.container}>
         
         {/* Names */}
         <div className={footerStyle.names}>
-          <span>Βασιλική</span>
+          <span>{t("footer.name1")}</span>
           <span className={footerStyle.and}>&</span>
-          <span>Στάθης</span>
+          <span>{t("footer.name2")}</span>
         </div>
 
         {/* Decorative line */}
